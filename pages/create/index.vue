8a48b8bc45664c50ce4602b3b52cf7bf1494f21e
Filiberto Reyes <filibertoreyes55@gmail.com>
2020-09-05 17:17:31 -0700

    test post and get methods

--- pages/create/index.vue
+++ pages/create/index.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>Create</div>
+  <div>
+    Create
+    <v-btn @click="onSubmit">Test firestore</v-btn>
+  </div>
 </template>
+<script>
+import axios from "axios";
+export default {
+  methods: {
+    onSubmit() {
+      axios
+        .post("https://agile-sprint-board.firebaseio.com/test.json", {
+          title: "hello"
+        })
+        .then(result => console.log(result))
+        .catch(e => console.log(e));
+    }
+  }
+};
+</script>
--- store/index.js
+++ store/index.js
@@ -1,17 +1,34 @@
 import Vuex from "vuex";
+import axios from "axios";
 
 const createStore = () => {
   return new Vuex.Store({
     state: {
       user: "TestUser",
-      loadedTickets: []
+      loadedTickets: [],
+      loadedTest: []
     },
     mutations: {
       setTickets(state, tickets) {
         state.loadedTickets = tickets;
+      },
+      setTest(state, test) {
+        state.loadedTest = test;
       }
     },
     actions: {
+      nuxtServerInit(vuexContext, context) {
+        return axios
+          .get("https://agile-sprint-board.firebaseio.com/test.json")
+          .then(res => {
+            const testArray = [];
+            for (const key in res.data) {
+              testArray.push({ ...res.data[key], id: key });
+            }
+            vuexContext.commit("setTest", testArray);
+          })
+          .catch(e => AudioContext.error(e));
+      },
       setTickets(vuexContext, tickets) {
         vuexContext.commit("setTickets", tickets);
       }
