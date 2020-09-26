8dc4a1c6375d93f26910cafec465ccb1142e02ed
Filiberto Reyes <filibertoreyes55@gmail.com>
2020-09-25 19:18:54 -0700

    testing project overview

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -4,7 +4,12 @@
     <h1>Current Project</h1>
     <Ticket-List :tickets="this.tickets" />
     <h1>Pie chart</h1>
-    <v-progress-circular :size="100" color="purple" :value="75" :width="20"></v-progress-circular>
+    <v-progress-circular
+      :size="100"
+      color="purple"
+      :value="75"
+      :width="20"
+    ></v-progress-circular>
   </div>
 </template>
 
@@ -15,28 +20,7 @@ export default {
     TicketList,
   },
   middleware: ["check-auth", "auth"],
-  // asyncData(context, callback) {
-  //   callback(null, {
-  //     tickets: [
-  //       {
-  //         title: "title 1",
-  //         description: "My description",
-  //         priority: "high",
-  //         status: "open",
-  //         type: "bug",
-  //         created: new Date(),
-  //       },
-  //       {
-  //         title: "title 2",
-  //         description: "My description",
-  //         priority: "high",
-  //         status: "open",
-  //         type: "bug",
-  //         created: new Date(),
-  //       },
-  //     ],
-  //   });
-  // },
+
   created() {
     this.$store.dispatch("setTickets", this.tickets);
   },
--- pages/projects/_id/overview.vue
+++ pages/projects/_id/overview.vue
@@ -1,7 +1,158 @@
 <template>
   <div>
-    <h1>Project Overview ID: {{ $route.params.id }}</h1>
-
     <nuxt-link to="/projects"><h2>Back to Projects</h2> </nuxt-link>
+
+    <v-sheet>
+      <v-sheet
+        class="pa-7 ticket-badge rounded-lg"
+        color="#4caf50"
+        max-width="100%"
+        elevation="5"
+        dark
+      >
+        <div class="text-h6">{{ project.name }}</div>
+      </v-sheet>
+
+      <v-simple-table fixed-header height="300px">
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">Name</th>
+              <th class="text-left">Email</th>
+              <th class="text-left">Role</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in desserts" :key="item.name">
+              <td>{{ item.name }}</td>
+              <td>{{ item.email || "No email" }}</td>
+              <td>{{ item.calories }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table></v-sheet
+    >
+
+    <v-sheet>
+      <v-sheet
+        class="pa-7 ticket-badge rounded-lg"
+        color="#4caf50"
+        max-width="100%"
+        elevation="5"
+        dark
+      >
+        <div class="text-h6">Assigned Personnel</div>
+      </v-sheet>
+
+      <v-simple-table fixed-header height="300px">
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">Name</th>
+              <th class="text-left">Email</th>
+              <th class="text-left">Role</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in desserts" :key="item.name">
+              <td>{{ item.name }}</td>
+              <td>{{ item.email || "No email" }}</td>
+              <td>{{ item.calories }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table></v-sheet
+    >
+
+    <v-spacer class="my-7"></v-spacer>
+
+    <v-sheet>
+      <v-sheet
+        class="pa-7 ticket-badge rounded-lg"
+        color="#4caf50"
+        max-width="100%"
+        elevation="5"
+        dark
+      >
+        <div class="text-h6">Tickets</div>
+      </v-sheet>
+      <v-simple-table fixed-header height="300px">
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">Name</th>
+              <th class="text-left">Email</th>
+              <th class="text-left">Role</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in desserts" :key="item.name">
+              <td>{{ item.name }}</td>
+              <td>{{ item.email || "No email" }}</td>
+              <td>{{ item.calories }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table></v-sheet
+    >
   </div>
 </template>
+
+<script>
+export default {
+  name: "projectOverview",
+  computed: {
+    project() {
+      return this.$store.getters.loadedProjects.find(
+        (project) => project.id == this.$route.params.id
+      );
+    },
+  },
+  data() {
+    return {
+      desserts: [
+        {
+          name: "Frozen Yogurt",
+          calories: 159,
+        },
+        {
+          name: "Ice cream sandwich",
+          calories: 237,
+        },
+        {
+          name: "Eclair",
+          calories: 262,
+        },
+        {
+          name: "Cupcake",
+          calories: 305,
+        },
+        {
+          name: "Gingerbread",
+          calories: 356,
+        },
+        {
+          name: "Jelly bean",
+          calories: 375,
+        },
+        {
+          name: "Lollipop",
+          calories: 392,
+        },
+        {
+          name: "Honeycomb",
+          calories: 408,
+        },
+        {
+          name: "Donut",
+          calories: 452,
+        },
+        {
+          name: "KitKat",
+          calories: 518,
+        },
+      ],
+    };
+  },
+};
+</script>
