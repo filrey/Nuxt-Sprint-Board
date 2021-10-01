ffcf764fd9b9cc3e5d2ad44e9521dfd9cc8d2062
Filiberto Reyes <filibertoreyes55@gmail.com>
2021-09-30 20:51:01 -0700

    additional user info in profile

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -5,27 +5,33 @@
     <v-row justify="center">
       <v-col cols="12" md="12">
         <v-card class="py-3 px-5">
-          <v-card-title>Profile</v-card-title>
+          <v-card-title>Basic Information</v-card-title>
           <v-row>
             <v-col cols="12" md="1">
               <v-avatar class="profile" color="grey" size="100" tile>
                 <v-img :src="user.photoUrl"></v-img>
               </v-avatar>
-              <v-chip class=" mt-3 d-flex justify-center" color="red" dark
-                >Administrator</v-chip
-              >
+              <v-chip class=" mt-3 d-flex justify-center" color="red" dark>{{
+                user.role
+              }}</v-chip>
             </v-col>
             <v-col cols="12" md="11">
-              <v-text-field
-                label="Name"
-                v-model="user.name"
-                disabled
-              ></v-text-field>
+              <v-text-field label="Name" v-model="user.name"></v-text-field>
               <v-text-field
                 label="Display Name"
                 v-model="user.displayName"
               ></v-text-field>
-              <v-btn @click="userTest()" color="primary">Find User</v-btn>
+              <v-text-field
+                label="Email"
+                v-model="user.email"
+                disabled
+              ></v-text-field>
+              <v-text-field
+                label="User ID"
+                v-model="user.uid"
+                disabled
+              ></v-text-field>
+              <v-btn color="primary">Edit</v-btn>
             </v-col>
           </v-row>
         </v-card>
