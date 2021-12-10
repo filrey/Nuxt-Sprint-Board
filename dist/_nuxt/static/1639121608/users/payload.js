d5d3a76bb562a4518e04bf393726093c9b6369f5
Filiberto Reyes <filibertoreyes55@gmail.com>
2021-12-09 23:48:13 -0800

    change to server target

--- dist/_nuxt/static/1639121608/users/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/users", (function(a,b,c){return {data:[],fetch:[],mutations:[[a,{message:"Session has expired",color:b}],["clearToken",c],["clearUser",c],[a,{message:"You must be logged in to view this page",color:b}]]}}("setSnackbar","error",void 0)));
